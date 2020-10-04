f0c537ce165664f3355f8de3b8a7390875873e17
K-SHIROSHITA <mi20a005@oecu.jp>
2020-10-04 16:29:46 +0900

    追加

---
+++ accuracy/acuracy.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.stats import zscore
+
+from botorch.models.pairwise_gp import PairwiseGP, PairwiseLaplaceMarginalLogLikelihood
+from botorch.fit import fit_gpytorch_model
+from botorch.acquisition import qNoisyExpectedImprovement, ExpectedImprovement, UpperConfidenceBound
+from botorch.optim import optimize_acqf
+import torch
+import gpytorch
+
+from accuracy.create_function import generate_data, generate_comparisons
+from accuracy.self_acquisition import MaxVariance
+
+def main(n, m, dim, noise, bounds):
+    train_X_random, train_X_mesh, train_y_random, train_y_mesh = generate_data(n, dim=dim)
+
+    train_comp_random = generate_comparisons(train_y_random, m, noise=noise)
+
+    covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims=dim))
+    model = PairwiseGP(train_X_random, train_comp_random, covar_module=covar_module)
+    mll = PairwiseLaplaceMarginalLogLikelihood(model)
+    mll = fit_gpytorch_model(mll)
+
+    qNEI = qNoisyExpectedImprovement(
+        model=model,
+        X_baseline=train_X_random.float()
+    )
+
+    EI =
+
+    next_X, acq_val = optimize_acqf(
+        acq_function=qNEI,
+        bounds=bounds,
+        q=2,
+        num_restarts=5,
+        raw_samples=256
+    )
+
+    points = torch.linspace(-1, 1, 101)
+    pred_y = zscore(model.posterior(points).mean.squeeze().detach().numpy())
+    train_y_mesh = zscore(train_y_mesh)
+
+    mse = np.square(train_y_mesh - pred_y).mean()
+
+    plt.clf()
+    plt.plot(train_X_mesh, train_y_mesh)
+    plt.plot(points, pred_y)
+
+    plt.show()
+    print("MSE", mse)
+    print(next_X)
+    print(acq_val)
+
+
+if __name__ == '__main__':
+    N = 100
+    M = 50
+    DIM = 1
+    NOISE = 0.1
+    BOUNDS = torch.stack([torch.zeros(DIM)-1,
+                          torch.ones(DIM)])
+
+    main(n=N, m=M, dim=DIM, noise=NOISE, bounds=BOUNDS)
---
+++ accuracy/create_function.py
@@ -0,0 +1,50 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+from itertools import combinations
+import numpy as np
+import torch
+import gpytorch
+
+
+def utility(X, covar_module):
+    """ Given X, output corresponding utility (i.e., the latent function)
+    """
+    covar = covar_module(X)
+    K = covar.numpy()
+    y = np.random.multivariate_normal(mean=np.zeros(len(X)), cov=K)
+
+    return y
+
+
+def generate_data(n, dim=1):
+    # ガウス過程に従う関数を作成する
+    covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims=1))
+    """ Generate data X and y """
+    # X is randomly sampled from dim-dimentional unit cube
+    # we recommend using double as opposed to float tensor here for
+    # better numerical stability
+    X_random = torch.rand(n, dim, dtype=torch.float64) * 2 - 1  # 書き換えてる
+    X_mesh = torch.linspace(-1, 1, 101).reshape(-1, 1)
+    X_all = torch.Tensor(np.vstack((X_random.numpy(), X_mesh.numpy())))
+    y_all = utility(X_all, covar_module)
+    y_random = y_all[:n]
+    y_mesh = y_all[n:]
+
+    return X_random, X_mesh, y_random, y_mesh
+
+
+def generate_comparisons(y, n_comp, replace=False, noise=None):
+    """  Create pairwise comparisons with noise """
+    # generate all possible pairs of elements in y
+    all_pairs = np.array(list(combinations(range(y.shape[0]), 2)))
+    # randomly select n_comp pairs from all_pairs
+    comp_pairs = all_pairs[np.random.choice(range(len(all_pairs)), n_comp, replace=replace)]
+    # add gaussian noise to the latent y values
+    c0 = y[comp_pairs[:, 0]] + np.random.standard_normal(len(comp_pairs)) * noise
+    c1 = y[comp_pairs[:, 1]] + np.random.standard_normal(len(comp_pairs)) * noise
+    reverse_comp = (c0 < c1)
+    comp_pairs[reverse_comp, :] = np.flip(comp_pairs[reverse_comp, :], 1)
+    comp_pairs = torch.tensor(comp_pairs).long()
+
+    return comp_pairs
